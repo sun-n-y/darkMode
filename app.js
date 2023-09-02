6fe05716fa66103e584ab2ce2d3a1f14d5978e73
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-02 15:01:39 -0400

    add dark mode feature to js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+//
+const toggleBtn = document.querySelector('.btn');
 
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+  if (document.documentElement.classList.contains('dark-theme')) {
+    toggleBtn.textContent = 'Light Mode';
+  } else {
+    toggleBtn.textContent = 'Dark Mode';
+  }
+});
