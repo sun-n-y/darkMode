6f5b594909d65d6fe501e96d3ded6c3208e8d561
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-02 15:23:26 -0400

    add dark mode complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //
 const toggleBtn = document.querySelector('.btn');
+const container = document.querySelector('.article-container');
 
 toggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-theme');
@@ -9,3 +10,17 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.textContent = 'Dark Mode';
   }
 });
+
+container.innerHTML = articles
+  .map(({ title, date, length, text }) => {
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `<article>
+            <h2 class="title">${title}</h2>
+            <div class="article-info">
+                <span>${formatDate}</span>
+                <span>${length} min read</span>
+            </div>
+            <p class="text">${text}</p>
+        </article>`;
+  })
+  .join('');
--- data.js
+++ data.js
@@ -2,25 +2,25 @@ const articles = [
   {
     title: 'the wet codebase',
     date: new Date(2023, 0, 1),
-    lenght: 11,
+    length: 11,
     text: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
   },
   {
     title: 'goodbye, clean code',
     date: new Date(2023, 1, 1),
-    lenght: 5,
+    length: 5,
     text: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
   },
   {
     title: 'my decade in review',
     date: new Date(2023, 2, 1),
-    lenght: 7,
+    length: 7,
     text: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
   },
   {
     title: 'what are the react team principles',
     date: new Date(2023, 3, 1),
-    lenght: 19,
+    length: 19,
     text: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
   },
 ];
